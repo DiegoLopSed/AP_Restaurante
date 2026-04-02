6ba67b146937a36a1de29bed04c899211adfb2ab
Diego Lopez Sedeño <diegolopsed160703@gmail.com>
2026-04-02 02:00:08 -0600

    Detalles por corte API

---
+++ backend/api/detalle_corte_caja.php
@@ -0,0 +1,24 @@
+<?php
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('log_errors', 1);
+
+ob_start();
+try {
+    require_once __DIR__ . '/../src/Controllers/DetalleCorteCajaController.php';
+    ob_clean();
+    $controller = new \App\Controllers\DetalleCorteCajaController();
+    $controller->handleRequest();
+} catch (Throwable $e) {
+    ob_clean();
+    http_response_code(500);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['success' => false, 'message' => 'Error: ' . $e->getMessage()], JSON_UNESCAPED_UNICODE);
+}
+ob_end_flush();
+
